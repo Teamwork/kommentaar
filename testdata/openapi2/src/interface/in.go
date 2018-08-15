d95d73d6abdfed6e053ca9ca4ba34173ef468430
Jordon Smith <jordon@jordonsmith.uk>
2018-08-15 08:03:38 +0100

    Add embedded interface to test case

--- testdata/openapi2/src/interface/in.go
+++ testdata/openapi2/src/interface/in.go
@@ -1,12 +1,17 @@
 package path
 
-import "interface/otherpkg"
+import (
+	"interface/otherpkg"
+	"io"
+)
 
 type resp struct {
 	Fooer          fooer       `json:"fooer"`
 	Fooers         []fooer     `json:"fooers"`
 	EmptyInterface interface{} `json:"emptyInterface"`
 	OtherPkg       otherpkg.I  `json:"otherPkg"`
+
+	io.Reader // ensure embedded interface doesn't error
 }
 
 // fooer is something.
