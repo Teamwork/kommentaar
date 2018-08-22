93bc005eac8f516a256c55e166fb0acb38377a6d
Martin Tournoij <martin@arp242.net>
2018-08-22 22:44:32 +0100

    Better test for kconfig.Load

--- kconfig/kconfig_test.go
+++ kconfig/kconfig_test.go
@@ -4,12 +4,32 @@ import (
 	"testing"
 
 	"github.com/teamwork/kommentaar/docparse"
+	"github.com/teamwork/test"
 )
 
-func TestLoadExample(t *testing.T) {
-	prog := docparse.NewProgram(false)
-	err := Load(prog, "../config.example")
-	if err != nil {
-		t.Fatalf("Load: %v", err)
+func TestLoad(t *testing.T) {
+	tests := []struct {
+		name string
+		in   []byte
+	}{
+		{"example", test.Read(t, "../config.example")},
+		{"default-response", []byte(test.NormalizeIndent(`
+			default-response 400: $ref: github.com/teamwork/kommentaar/docparse.Param
+			default-response 404 (application/json): $ref: net/mail.Address
+		`))},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f, clean := test.TempFile(t, string(tt.in))
+			defer clean()
+
+			prog := docparse.NewProgram(false)
+
+			err := Load(prog, f)
+			if err != nil {
+				t.Fatalf("Load: %v", err)
+			}
+		})
 	}
 }
